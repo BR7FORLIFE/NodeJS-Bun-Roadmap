1f49b6f47026a31333db6240fdbcab4621fee480
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-01-18 18:50:20 -0500

    feat: defined the structure for create the app and use the express framework

--- 01-library-bun/app.ts
+++ 01-library-bun/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import bookRoutes from "./src/routes/book-routes";
 
 const app = express();
 
 app.use(express.json());
+app.use("/books", bookRoutes);
 
 export default app;
