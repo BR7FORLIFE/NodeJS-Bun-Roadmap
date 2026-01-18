d9979958a4fc5c6e46cb8afd7490b2c86523c17d
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-01-18 18:50:43 -0500

    feat. define the singleton conection for mongodb

--- 01-library-bun/src/config/connectDB.ts
+++ 01-library-bun/src/config/connectDB.ts
@@ -1,9 +1,18 @@
-import mongoose from "mongoose";
+import { MongoClient, Db } from "mongodb";
 
-export async function connectDB() {
-    try {
-        await mongoose.connect(process.env.MONGO_URL!);
-    } catch (error) {
-        process.exit(1);
-    }
+const URI = process.env.MONGO_URI!;
+const DB_NAME = process.env.MONGO_DB!;
+
+let client: MongoClient | null = null;
+let db: Db | null = null;
+
+export async function connectDB(): Promise<Db> {
+  if (!client) {
+    client = new MongoClient(URI);
+    await client.connect();
+    db = client.db(DB_NAME);
+    console.log("✅ Mongo conectado");
+  }
+
+  return db!;
 }
